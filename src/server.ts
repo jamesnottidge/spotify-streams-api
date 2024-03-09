983aca7831d704662c4c37221b7703fd02ab3b7f
jamesnottidge <freshice57@gmail.com>
2024-03-09 16:27:38 +0100

    basic router setup

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,28 @@
 import express from 'express';
 import { IncomingMessage } from 'node:http';
+import morgan from 'morgan';
+import router from './router';
 
 
 const app = express();
+
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+
+
+
+
 
 app.get('/', (req: IncomingMessage, res: any) => {
     res.send('Hello World dwellers!');
 });
+
+
+
+app.use('/api/v1', router);
 
 export default app;
